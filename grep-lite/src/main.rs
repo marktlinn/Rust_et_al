1b175eb5b73bafdbb9bd201bf1785a9c44071723
marktlinn <marktlinn@gmail.com>
2023-11-19 19:17:35 +0100

    add: input argument parsing

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use clap::{App,Arg};
 
 fn search(search_string: &str, quote: &str) -> Vec<String>{
   let re = Regex::new(search_string).unwrap();
@@ -16,13 +17,23 @@ fn search(search_string: &str, quote: &str) -> Vec<String>{
 }
 
 fn main() {
+  let args = App::new("GREP-Lite")
+    .version("1.0")
+    .author("Mark")
+    .about("Searches for patterns in files")
+    .arg(Arg::with_name("pattern")
+      .help("The pattern to search for")
+      .takes_value(true)
+      .required(true))
+    .get_matches();
+
   let quote = "Every face, every shop, bedroom window, public-house, and
   dark square is a picture feverishly turned--in search of what?
   It is the same with books. What do we seek through millions of pages?";
 
-  let search_string = "of";
+  let pattern = args.value_of("pattern").unwrap();
 
-  let result = search(search_string, quote);
+  let result = search(pattern, quote);
   for line in result {
     println!("{}", line);
   }
